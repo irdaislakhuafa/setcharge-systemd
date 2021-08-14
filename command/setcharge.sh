3ee6a8531bf16bfed41923efc0cd5497d9b50f1b
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2021-08-14 22:57:16 +0700

    Add 'show-limit' option to show current battery limit

--- command/setcharge.sh
+++ command/setcharge.sh
@@ -2,9 +2,11 @@
 user=$(whoami)
 
 if [ $user == "root" ]; then 
-	if [ $1 -le 100 ]; then 
+	if [ $1 -le 100 2> /dev/null ]; then 
 		echo $1 > /etc/.battery.conf &!
 		systemctl restart setcharge.service &!
+	elif [ "$1" == "show-limit" ]; then
+		echo "Battery limit : $(cat /sys/class/power_supply/BAT0/charge_control_end_threshold)"
 	else 
 		echo "Enter the number between 1 and 100!"
 	fi
