cb0205b1777b2a1828e346ecad8f305ab64dca3a
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2021-08-19 14:49:26 +0700

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-if [ $(whoami) == "root" ]; then
+if [ "$(whoami)" == "root" ]; then
 	cp -rv command/setcharge.sh /usr/bin/setcharge
 	cp -rv services/setcharge.service /usr/lib/systemd/system/setcharge.service
 	cp -rv config/.battery.conf /etc/.battery.conf 
